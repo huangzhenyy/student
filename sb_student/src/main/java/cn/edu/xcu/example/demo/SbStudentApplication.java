f8a7d54575f9f0ff180d3861b0ead180cc18dda5
Huang <Huang@DELL>
2019-11-15 17:01:58 +0800

    学生管理系统项目

---
+++ sb_student/src/main/java/cn/edu/xcu/example/demo/SbStudentApplication.java
@@ -0,0 +1,13 @@
+package cn.edu.xcu.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbStudentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbStudentApplication.class, args);
+	}
+
+}
